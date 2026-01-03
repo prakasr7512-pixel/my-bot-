6f58d4cf9e35d0246e698c8950193b91a29ed854
prakasr7512-pixel <prakasr7512@gmail.com>
2026-01-03 23:50:28 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -229,7 +229,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/P_Amedia">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/pamoviesrequest">[ P A MOVIES ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
