ebd64d06040f4d2c79dc09669de28bbf63f45a31
prakasr7512-pixel <prakasr7512@gmail.com>
2026-07-31 19:34:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,8 +55,8 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002585782108')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/pa_backup')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-100")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-100")# Channels for force sub (make sure bot is admin)
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003994423217")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_channels     = environ.get("AUTH_CHANNELS", "-1002967864116")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
